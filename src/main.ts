b1989d7716944a04210dbfc8114b7986afd68105
Diana Marin <dipamato@gmail.com>
2024-11-25 17:18:19 -0500

    cambio vercel 5

--- src/main.ts
+++ src/main.ts
@@ -16,14 +16,19 @@ const uploadMiddleware = multer({ storage });
 app.use(uploadMiddleware.any());
 
 
-  app.enableCors()
+app.enableCors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+});
   app.useGlobalPipes(new ValidationPipe());
   // Aumenta el límite de tamaño de la entidad de la solicitud a 10MB
   app.use(bodyParser.json({ limit: '10mb' }));
 
 
  
-
-  await app.listen(3000);
+ // Puerto dinámico para Vercel
+ const port = process.env.PORT || 3000;
+ await app.listen(port);
 }
 bootstrap();
